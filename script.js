46bbe583b56b40587869301692c59fe6a00a4f6a
aaronbadger <63116138+aaronbadger@users.noreply.github.com>
2023-07-19 14:46:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 import marked from "https://unpkg.com/marked@2.0.0/lib/marked.esm.js";
 import { endpoint } from "https://cdn.skypack.dev/@octokit/endpoint";
 
-const TOKEN = "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"; // Your GitHub token (consider handling this server-side)
+const TOKEN = ""; // Your GitHub token (consider handling this server-side)
 
 async function fetchIssues() {
   const { url, ...options } = endpoint("GET /repos/:owner/:repo/issues", {
@@ -33,15 +33,22 @@ function createIssueHeader(issue) {
 }
 
 function createIssueBody(issue) {
-  const { number, body } = issue;
+  const { number, body, labels, state, created_at, assignee, user } = issue;
   const bodyElement = document.createElement("div");
   bodyElement.classList.add("issue-body");
   bodyElement.id = `issue-body-${number}`;
   bodyElement.style.display = "none";
-  
-  // Render the Markdown and set it as innerHTML
-  bodyElement.innerHTML = marked(body);
-  
+
+  // Render the additional information
+  bodyElement.innerHTML = `
+    <p><strong>Labels:</strong> ${labels.map((label) => label.name).join(", ")}</p>
+    <p><strong>State:</strong> ${state}</p>
+    <p><strong>Created At:</strong> ${created_at}</p>
+    <p><strong>Assignee:</strong> ${assignee ? assignee.login : "None"}</p>
+    <p><strong>Reporter:</strong> ${user.login}</p>
+    <div>${marked(body)}</div>
+  `;
+
   return bodyElement;
 }
 
